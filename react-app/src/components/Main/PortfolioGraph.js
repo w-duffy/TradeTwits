779a7fba3727caeb2ee8383066da5de355e43ced
will <w.duffy@outlook.com>
2022-02-15 15:08:31 -0600

    created is loaded for the home page

--- react-app/src/components/Main/PortfolioGraph.js
+++ react-app/src/components/Main/PortfolioGraph.js
@@ -4,7 +4,8 @@ import Graph from './Graph';
 import {getPortfolioDetails} from '../../store/portfolio'
 
 function PortfolioGraph(){
-
+    const [isLoaded, setIsLoaded] = useState(false)
+    // const [portDetails, setPortDetails] = useState()
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
     const portfolioDetail = useSelector(state => state.portfolioReducer.portfolioDetail)
@@ -15,18 +16,21 @@ function PortfolioGraph(){
           console.log("ID IN COMPON", id)
         async function getDetails() {
             await dispatch(getPortfolioDetails(id))
+            setIsLoaded(true)
+            console.log("PORTFOLIO INFO IN COMPN", portfolioDetail.info)
+            // setPortDetails(portfolioDetail.info)
           }
         getDetails()
 
     }, [])
-    console.log("PORTFOLIO INFO IN COMPN", portfolioDetail.info)
 
       //for the slice make the second number a variable that will change with a useEffect when the user clicks how many days to view.
 
 
+    if (isLoaded){
 
-    return (
-        <div>
+        return (
+            <div>
             {portfolioDetail.info.map(detail => (
                 <>
                 {detail.ticker} {Number(detail.values[0]).toFixed(2)}
@@ -35,6 +39,7 @@ function PortfolioGraph(){
             ))}
         </div>
     )
+} else return(<></>)
 }
 
 export default PortfolioGraph
